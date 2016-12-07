fb9f8cb781dd5fff85cf24fbabf8c160088a4779
Alex Barcelo <alex@betarho.net>
2016-12-07 10:41:55 +0100

    update everything to newer versions and use two docker tags (3.0 and 3.1)

---
+++ 3.1/docker-entrypoint.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+set -e
+
+# In all scenarios, proceed to prepare the settings file
+cat << EOF > /etc/mailman.cfg
+[mailman]
+# This address is the "site owner" address.
+# It should point to a human.
+site_owner: ${MAILMAN_SITE_OWNER}
+
+[paths.here]
+# Everything in the same directory
+var_dir: /opt/mailman/var
+
+[archiver.hyperkitty]
+class: mailman_hyperkitty.Archiver
+enable: ${HYPERKITTY_ENABLE}
+configuration: /etc/hyperkitty.cfg
+
+[database]
+class: mailman.database.postgresql.PostgreSQLDatabase
+url: postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}
+
+[mta]
+incoming: mailman.mta.postfix.LMTP
+outgoing: mailman.mta.deliver.deliver
+lmtp_host: `hostname`
+lmtp_port: 8024
+smtp_host: ${POSTFIX_HOST}
+smtp_port: ${POSTFIX_PORT}
+configuration: python:mailman.config.postfix
+
+[webservice]
+hostname: `hostname`
+port: 8001
+use_https: no
+admin_user: ${MAILMAN_ADMIN_USER}
+admin_pass: ${MAILMAN_ADMIN_PASSWORD}
+EOF
+
+cat << EOF > /etc/hyperkitty.cfg
+[general]
+base_url: http://${HYPERKITTY_HOST}:${HYPERKITTY_PORT}/archives
+api_key: ${HYPERKITTY_ARCHIVER_API_KEY}
+EOF
+
+chown -R mailman:mailman /opt/mailman
+
+if [ "$1" = 'start' ]; then
+	# Main run behaviour, which is done directly to the master
+	# because we want it in the foreground, not background
+	shift
+	exec gosu mailman master "$@"  # note that mailman is the user, master the command
+fi
+
+if [ "$1" = 'stop' ] ; then
+	# This is not allowed
+	echo "Docker does not support `stop`"
+	exit 2
+fi
+
+for word in "help aliases conf create import21 info inject lists members qfile remove reopen restart shell status unshunt version withlist" ; do
+	if [ "$1" = "$word" ] ; then
+		gosu mailman exec mailman "$@"
+	fi
+done
+
+# It doesn't seem a mailman subcommand, execute it in the shell directly
+exec "$@"
